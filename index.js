da2369f846d48fba5bd2bd34b0db17bee540e532
GregoriusRafaelS <rafaelsantosa75@gmail.com>
2024-05-16 13:40:44 +0700

    Add app1 Node.js application

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello from App1!');
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+  console.log(`App1 listening on port ${port}`);
+});
