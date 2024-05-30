776d01d8d715e65b5c3d50aea2814b9babd4a16b
RianBudiyanto44 <rianbudiyanto44@gmail.com>
2024-05-30 12:15:14 +0700

    update index.js dan cloudbuild.yaml

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
+// const express = require('express');
+// const app = express();
+
+// app.get('/', (req, res) => {
+//   res.send('Hello this is App 1, Enjoy ur day');
+// });
+
+// const port = process.env.PORT || 8080;
+// app.listen(port, () => {
+//   console.log(`App1 listening on port ${port}`);
+// });
+
 const express = require('express');
+const sequelize = require('./db');
+const dotenv = require('dotenv');
+
+dotenv.config();
+console.log(process.env.DB_NAME);
+
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {
-  res.send('Hello this is App 1, Enjoy ur day');
-});
+app.use('/api/auth', require('./routes/auth'));
 
-const port = process.env.PORT || 8080;
-app.listen(port, () => {
-  console.log(`App1 listening on port ${port}`);
+const PORT = process.env.PORT || 8080;
+
+sequelize.sync().then(() => {
+  console.log(process.env.DB_PASSWORD);
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}).catch(err => {
+  console.error('Unable to connect to the database:', err);
 });
