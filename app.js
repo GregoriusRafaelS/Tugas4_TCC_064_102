ef5647ab0865a0a4af1dda71ab3f10ed58aa167a
RianBudiyanto44 <rianbudiyanto44@gmail.com>
2024-06-01 19:51:36 +0700

    test app.js 2

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const connection = require('./db');
 
 const app = express();
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 4000;
 
 app.use(bodyParser.json());
 
