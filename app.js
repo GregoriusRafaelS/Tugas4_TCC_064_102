50133bb3e78b7ea2435cc0a47f982785751fb895
RianBudiyanto44 <rianbudiyanto44@gmail.com>
2024-06-02 18:09:07 +0700

    coba app.js 7

--- app.js
+++ app.js
@@ -1,3 +1,21 @@
+// const express = require('express');
+// const path = require('path');
+// const expressPhp = require('express-php');
+
+// const app = express();
+// const port = process.env.PORT || 8080;
+
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'index.html'));
+// });
+
+// // Menangani semua permintaan PHP di dalam folder 'auth'
+// app.use('/auth', expressPhp.cgi(path.join(__dirname, 'auth')));
+
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
+
 const express = require('express');
 const path = require('path');
 const expressPhp = require('express-php');
@@ -5,12 +23,13 @@ const expressPhp = require('express-php');
 const app = express();
 const port = process.env.PORT || 8080;
 
+// Serve index.html dari direktori yang sama dengan app.js
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 // Menangani semua permintaan PHP di dalam folder 'auth'
-app.use('/process', expressPhp.cgi(path.join(__dirname, 'process')));
+app.use('/auth', expressPhp.cgi(path.join(__dirname, 'auth')));
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
