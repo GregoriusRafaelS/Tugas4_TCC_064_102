64fd84c996d01f2c1e9ca0fa1afe24099e521cf6
RianBudiyanto44 <rianbudiyanto44@gmail.com>
2024-06-02 21:28:41 +0700

    update db

--- db.js
+++ db.js
@@ -1,10 +1,16 @@
 const mysql = require('mysql2');
 
+// const connection = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME
+// });
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+  host: '34.170.235.0',
+  user: 'tcc',
+  password: 'tccprojectmantap',
+  database: 'tcc'
 });
 
 connection.connect((err) => {
