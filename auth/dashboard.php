4c89a5793db365d931b1b141a2490ba48f2125c0
RianBudiyanto44 <rianbudiyanto44@gmail.com>
2024-06-02 22:39:12 +0700

    add dashboard

--- auth/dashboard.php
+++ auth/dashboard.php
@@ -3,9 +3,43 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>dashboard</title>
+    <title>Dashboard</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
 </head>
 <body>
-    <h1>berhasil</h1>
+    <div class="container-sm">
+        <h1 class="text-center mt-3 mb-3">Catatan Keuangan</h1>
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                  <th scope="col">Tanggal</th>
+                  <th scope="col">Nama</th>
+                  <th scope="col">Value</th>
+                  <th scope="col">Type</th>
+                </tr>
+              </thead>
+              <tbody>
+              <?php
+                    // Ambil data dari API
+                    $url = "https://app2-z4pp7wkg3a-uc.a.run.app/data";
+                    $json = file_get_contents($url);
+                    $data = json_decode($json, true);
+
+                    // Tampilkan data ke dalam tabel
+                    foreach ($data as $row) {
+                        echo "<tr>";
+                        echo "<td>" . $row['tanggal'] . "</td>";
+                        echo "<td>" . $row['name'] . "</td>";
+                        echo "<td>" . $row['value'] . "</td>";
+                        echo "<td>" . $row['type'] . "</td>";
+                        echo "</tr>";
+                    }
+                ?>
+              </tbody>
+        </table>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
 </body>
 </html>
