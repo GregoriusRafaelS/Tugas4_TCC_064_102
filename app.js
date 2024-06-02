55a2ac6db520fc001fb7c4afc24fe0036862bc0b
RianBudiyanto44 <rianbudiyanto44@gmail.com>
2024-06-02 18:43:41 +0700

    coba frontend

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-// const connection = require('./db');
+const path = require('path');
+const expressPhp = require('express-php');
 
 const app = express();
 const port = process.env.PORT || 8080;
 
+// Serve index.html dari direktori yang sama dengan app.js
 app.get('/', (req, res) => {
-  res.send('coba');
+  res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+// Menangani semua permintaan PHP di dalam folder 'auth'
+// app.use('/auth', expressPhp.cgi(path.join(__dirname, 'auth')));
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
