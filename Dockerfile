0d967962ab532d712e82d0c081850d6bb0b13cce
RianBudiyanto44 <rianbudiyanto44@gmail.com>
2024-06-04 07:00:59 +0700

    hapus comment

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,3 @@
-# FROM node:18
-# WORKDIR /app
-# COPY package*.json ./
-# RUN npm install
-# COPY . .
-# EXPOSE 4000
-# CMD ["npm", "start"]
 
 # Menggunakan node image dari Docker Hub
 FROM node:18
--- app.js
+++ app.js
@@ -1,31 +1,3 @@
-// const express = require('express');
-// const bodyParser = require('body-parser');
-// const connection = require('./db');
-
-// const app = express();
-// const port = process.env.PORT || 4000;
-
-// app.use(bodyParser.json());
-
-// app.get('/login', (req, res) => {
-//   const query = 'SELECT * FROM users';
-//   connection.query(query, (err, results) => {
-//     if (err) {
-//       console.error('Error fetching data from database:', err);
-//       res.status(500).send('Internal Server Error');
-//     } else {
-//       res.json(results);
-//     }
-//   });
-// });
-
-// //api register
-
-
-// app.listen(port, () => {
-//   console.log(`Server is running on port ${port}`);
-// });
-
 // app.js
 const express = require('express');
 const bodyParser = require('body-parser');
